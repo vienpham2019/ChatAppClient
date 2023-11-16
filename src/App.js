710c8de7613261e5fbaa88d6751c4335d51dcaf6
vien pham <vienpham@viens-Air.home>
2023-11-16 15:54:54 -0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <h1>Hi</h1>;
-}
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import NotFound from "./pages/NotFound/NotFound";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+
+        {/* catch all - replace with 404 component */}
+        {/* replace is a bad request to good request in history */}
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
