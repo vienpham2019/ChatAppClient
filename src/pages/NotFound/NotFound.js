33102b7d345db3b01b8978b00664fc63f7898487
vien pham <vienpham@viens-Air.home>
2023-11-16 22:51:09 -0600

    nofound page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,27 @@
+import notfoundimg from "../../images/pagenotfound.png";
+import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
-    <div className="bg-neutral-400 text-center">
-      <h1 className="text-3xl font-bold text-yellow-200">Hello world!</h1>
+    <div className="flex justify-center items-center h-screen ">
+      <div className="flex flex-col items-center gap-5 text-center px-2">
+        <img
+          className="w-64 md:w-96 "
+          src={notfoundimg}
+          alt="not found image"
+        />
+        <span className="text-base tracking-tight uppercase md:text-2xl">
+          oops, sorry we can't find that page
+        </span>
+        <span className="text-slate-500 ">
+          Either something went wrong or the page doesn't exist anymore.
+        </span>
+        <Link
+          to={"/login"}
+          className="border text-center text-slate-200 rounded bg-cyan-600 w-40 py-2 hover:bg-cyan-800"
+        >
+          Go to Home
+        </Link>
+      </div>
     </div>
   );
 };
